945a0eb55dfa93d76bb886e328d254d768501339
pankaj <88972716+pankajsahh@users.noreply.github.com>
2022-06-04 06:17:20 +0100

    backend

--- faculty_pbl/settings.py
+++ faculty_pbl/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from msilib.schema import Component
 from pathlib import Path
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -156,5 +155,3 @@ EMAIL_HOST_PASSWORD = '1111'
 ALLOWED_HOSTS=['*']
 
 CORS_ORIGIN_ALLOW_ALL = True
-
-django_heroku.settings(locals())
